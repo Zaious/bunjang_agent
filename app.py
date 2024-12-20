e9187b7db261cddd68f07a10a3d97a4dd1dac3fb
Zaious(AIGC Design) <zaious.design@gmail.com>
2024-12-21 00:48:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -154,5 +154,5 @@ def scrape_product():
         if driver:
             driver.quit()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
